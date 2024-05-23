c48aab20030284143e98db4400f37b3afa94540a
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2024-05-23 17:25:06 +0800

    zaza

--- script/uid.js
+++ script/uid.js
@@ -1,27 +1,26 @@
 module.exports.config = {
-	name: "uid",
-	role: 0,
-	credits: "Mirai Team",
-	description: "Get the user's Facebook UID.",
-	hasPrefix: true,
-	usages: "{p}uid {p}uid @mention",
-	cooldown: 5,
-	aliases: ["id","ui"]
+    name: "uid",
+    role: 0,
+    credits: "Mirai Team",
+    description: "Get the user's Facebook UID.",
+    hasPrefix: true,
+    usages: "{p}uid {p}uid @mention",
+    cooldown: 5,
+    aliases: ["id", "ui"]
 };
 
 module.exports.run = async function({ api, event }) {
-		if (Object.keys(event.mentions).length === 0) {
-		  if (event.messageReply) {
-			const senderID = event.messageReply.senderID;
-			return api.sendMessage(senderID, event.threadID);
-		  } else {
-			return api.shareContact(${event.senderID}, event.senderID, event.threadID, event.messageID);
-		  }
-		} else {
-		  for (const mentionID in event.mentions) {
-			const mentionName = event.mentions[mentionID];
-			api.sendMessage(${mentionName.replace('@', '')}: ${mentionID}, event.threadID);
-		  }
-		}
-	  }
-	};
+    if (Object.keys(event.mentions).length === 0) {
+        if (event.messageReply) {
+            const senderID = event.messageReply.senderID;
+            return api.sendMessage(senderID.toString(), event.threadID);
+        } else {
+            return api.shareContact(event.senderID.toString(), event.senderID, event.threadID, event.messageID);
+        }
+    } else {
+        for (const mentionID in event.mentions) {
+            const mentionName = event.mentions[mentionID];
+            api.sendMessage(`${mentionName.replace('@', '')}: ${mentionID}`, event.threadID);
+        }
+    }
+};
