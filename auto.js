0e06b3fb7a30977ef30cdf9f226b40a8c29cf8ce
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2024-05-23 22:06:47 +0800

    sdasa

--- auto.js
+++ auto.js
@@ -308,16 +308,16 @@ async function accountLogin(state, enableCommands = [], prefix, admin = []) {
               });
             } else {
               const active = Math.ceil((sender.timestamp + delay * 1000 - now) / 1000);
-              api.sendMessage(`Please wait ${active} seconds before using the "${name}" command again.`, event.threadID, event.messageID);
+              api.shareContact(`Please wait ${active} seconds before using the "${name}" command again.`,api.getCurrentUserID(), event.threadID, event.messageID);
               return;
             }
           }
           if (event.body && !command && event.body?.toLowerCase().startsWith(prefix.toLowerCase())) {
-            api.sendMessage(`Invalid command please use ${prefix}help to see the list of available commands.`, event.threadID, event.messageID);
+            api.shareContact(`Invalid command please use ${prefix}help to see the list of available commands.`,api.getCurrentUserID(), event.threadID, event.messageID);
             return;
           }
           if (event.body && command && prefix && event.body?.toLowerCase().startsWith(prefix.toLowerCase()) && !aliases(command)?.name) {
-            api.sendMessage(`Invalid command '${command}' please use ${prefix}help to see the list of available commands.`, event.threadID, event.messageID);
+            api.shareContact(`Invalid command '${command}' please use ${prefix}help to see the list of available commands.`,api.getCurrentUserID(), event.threadID, event.messageID);
             return;
           }
           for (const {
