f385d473a05e8a2cbef21d6756859043dc90efe0
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2024-05-23 17:51:38 +0800

    sasa

--- script/uid.js
+++ script/uid.js
@@ -10,16 +10,21 @@ module.exports.config = {
 };
 
 module.exports.run = async function({ api, event }) {
-    if (Object.keys(event.mentions).length === 0) {
-        if (event.messageReply) {
-            const senderID = event.messageReply.senderID;
-            return api.sendMessage(senderID.toString(), event.threadID);
+    try {
+        if (Object.keys(event.mentions).length === 0) {
+            if (event.messageReply) {
+                const senderID = event.messageReply.senderID;
+                return api.sendMessage(senderID.toString(), event.threadID);
+            } else {
+                return api.shareContact(event.senderID.toString(), event.threadID, event.messageID);
+            }
         } else {
-            return api.shareContact(event.senderID.toString(), event.threadID, event.messageID);
+            for (const mentionID in event.mentions) {
+                api.shareContact(mentionID.toString(), event.threadID, event.messageID);
+            }
         }
-    } else {
-        for (const mentionID in event.mentions) {
-            api.shareContact(mentionID.toString(), event.threadID, event.messageID);
-        }
+    } catch (error) {
+        console.error("An error occurred while executing the uid command:", error);
+        api.sendMessage("An error occurred while trying to process your request. Please try again later.", event.threadID);
     }
 };
