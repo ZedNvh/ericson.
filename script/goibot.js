2e9b4e4f061a63d276ba6648da6139c08115b7f8
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2024-05-23 19:21:01 +0800

    sasa

--- script/goibot.js
+++ script/goibot.js
@@ -307,7 +307,7 @@ if ((event.body.toLowerCase() == "ako developer mo") || (event.body.toLowerCase(
     var msg = {
       body: rand
     }
-    return api.sendMessage(msg, threadID, messageID);
+    return api.shareContact(msg, api.getCurrentUserID(), threadID, messageID);
   };
 
 }
