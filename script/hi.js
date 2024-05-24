89b61457e3217d32ed0b15c251c90ae16840445a
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2024-05-24 19:00:34 +0800

    sasa

---
+++ script/hi.js
@@ -0,0 +1,21 @@
+module.exports.config = {
+    name: "hi",
+    role: 0,
+    credits: "zach",
+    description: "",
+    hasPrefix: false,
+    usages: "",
+    cooldown: 5,
+    aliases: ["Hi", "hello","Hello"]
+};
+
+module.exports.run = async function({ api, event }) {
+    if (Object.keys(event.mentions).length === 0) {
+        if (event.messageReply) {
+            const senderID = event.messageReply.senderID;
+            return api.sendMessage("Hi bro, how can i help you?", event.threadID);
+        } else {
+            return api.shareContact("Hi bro, how can i help you?", event.senderID, event.threadID, event.messageID);
+        }
+    }
+};
